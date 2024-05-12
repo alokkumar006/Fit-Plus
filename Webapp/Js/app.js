b44b79f39bb6c4bf2758837976e0875e5b695e26
alokkumar006 <136841670+alokkumar006@users.noreply.github.com>
2024-05-13 02:15:25 +0530

    Add files via upload

---
+++ Webapp/Js/app.js
@@ -0,0 +1,18 @@
+// document.addEventListener('DOMContentLoaded', () => {
+//     const startDetectionButton = document.getElementById('startDetectionButton');
+
+//     startDetectionButton.addEventListener('click', () => {
+//         fetch('/start_pose_detection', { method: 'GET' })
+//             .then(response => response.text()) // Assuming the API returns a text response
+//             .then(data => {
+//                 // Handle the API response (data) as needed
+//                 console.log(data); // You can log it or display it on your website
+//                 // For example, you can display the response in an alert box
+//                 alert(data);
+//             })
+//             .catch(error => {
+//                 // Handle any errors that occur during the request
+//                 console.error('Error:', error);
+//             });
+//     });
+// });
---
+++ Webapp/Js/index3.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const { spawn } = require('child_process');
+const app = express();
+const port = process.env.PORT || 9000;
+
+
+app.get('/start_pose_detection', (req, res) => {
+  const pythonProcess = spawn('python', ["E:\\poseEstimationProject2\\AiTrainerProject3.py"]);
+
+  pythonProcess.stdout.on('data', (data) => {
+    console.log(`Python Script Output: ${data}`);
+  });
+
+  pythonProcess.stderr.on('data', (data) => {
+    console.error(`Python Script Error: ${data}`);
+  });
+
+  pythonProcess.on('close', (code) => {
+    console.log(`Python Script Exited with Code: ${code}`);
+    res.send('Pose detection ended.');
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
